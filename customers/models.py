6f9866757c244574c575b9054a71b9717ef2356e
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-03-27 23:04:58 +0600

    Create a project and add users

---
+++ customers/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True)
