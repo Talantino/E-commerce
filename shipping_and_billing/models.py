03dc205447aa94032d2b16df76d447078b636b00
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-03-28 23:36:43 +0600

    Import missing validator

--- shipping_and_billing/models.py
+++ shipping_and_billing/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from customers.models import User
 from django.conf import settings
+from django.core.validators import RegexValidator
 
 
 class Address(models.Model):
