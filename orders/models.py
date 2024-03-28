eaa1b178b3da595d3c2773e9d60f7a941cc30143
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-03-28 12:55:05 +0600

    add models for orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from customers.models import User
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled')
+    ]
+    PAYMENT_METHOD_CHOICES = [
+        ('Card', 'Card'),
+        ('Cash', 'Cash')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    shipping_address = models.ForeignKey(
+        "Address",
+        related_name="shipping_address",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    billing_address = models.ForeignKey(
+        "Address",
+        related_name="billing_address",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    payment_method = models.CharField(max_length=4, choices=PAYMENT_METHOD_CHOICES)
+    discount_applied = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order by {self.user}"
+
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order details of the {self.order}"
